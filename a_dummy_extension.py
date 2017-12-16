8315c19ae25961e018ec5f585fffcd4e6a9257da
Brenden Romanowski <brenden.romanowski@gmail.com>
2017-12-15 22:02:41 -0500

    Basic GIF creation working. Lots of improvements needed. Currently outputting to hardcoded locations.

--- a_dummy_extension.py
+++ a_dummy_extension.py
@@ -4,9 +4,12 @@ Dummy extension for Inkscape for experimentation
 """
 import os
 import inkex
+import subprocess
+import imageio
 from simplestyle import *
+from lxml import etree
 
-os.environ["TCL_LIBRARY"] = os.path.join(os.getcwd(), "..\\..\\lib\\python2.7\\tcl\\tcl8.5")
+'''os.environ["TCL_LIBRARY"] = os.path.join(os.getcwd(), "..\\..\\lib\\python2.7\\tcl\\tcl8.5")
 os.environ["TK_LIBRARY"] = os.path.join(os.getcwd(), "..\\..\\lib\\python2.7\\tcl\\tk8.5")
 
 from Tkinter import Tk, Label, Button
@@ -30,8 +33,14 @@ class MyFirstGUI:
 
 root = Tk()
 my_gui = MyFirstGUI(root)
-root.mainloop()
+root.mainloop()'''
 
+SVG_FILE = "C:\\Users\\Owner\\Desktop\\ball.svg"
+GIF_FILE = "C:\\Users\\Owner\\Desktop\\ball.gif"
+PREFIX = "C:\\Users\\Owner\\Desktop\\Output\\"
+INKSCAPE_LABEL_TAG = "{http://www.inkscape.org/namespaces/inkscape}label"
+ANIMATION_LAYER_PREFIX = "Animation Layer "
+LAYER_TAG = "{http://www.w3.org/2000/svg}g"
 
 class DummyExtension(inkex.Effect):
     """
@@ -45,37 +54,96 @@ class DummyExtension(inkex.Effect):
             '--count', action='store', type='int',
             dest='count', default=25, help='Dividers (Length axis)')
 
+    def log(self, message):
+        with(open('C:/Users/Owner/Desktop/debug.txt', 'w')) as _:
+            _.write(message)
+
     def effect(self):
         count = self.options.count
-        
-        with(open('C:/Users/Owner/Desktop/debug.txt', 'w')) as _:
-            _.write('Number: %d\n' % count)
 
         # Get access to main SVG document element and get its dimensions.
-        svg = self.document.getroot()
+        root = self.document.getroot()
+        children = root.getchildren()
 
-        # Get the attributes:
-        width  = self.unittouu(svg.get('width'))
-        height = self.unittouu(svg.attrib['height'])
+        layers = {}
 
-        # Create a new layer.
-        layer = inkex.etree.SubElement(svg, 'g')
-        layer.set(inkex.addNS('label', 'inkscape'), 'newlayer')
-        layer.set(inkex.addNS('groupmode', 'inkscape'), 'layer')
+        for i in range(0, len(children)):
+            if children[i].tag != LAYER_TAG:
+                    continue
 
-        # Create an SVG text element
-        text = inkex.etree.Element(inkex.addNS('text','svg'))
-        text.text = 'Hello %s!' % (count)
+            label = children[i].get(INKSCAPE_LABEL_TAG)
+            if ANIMATION_LAYER_PREFIX in label:
+                layerNumber = label[len(ANIMATION_LAYER_PREFIX):]
+                layers[layerNumber] = i
+            
+        '''
+        Create a png for each layers
+        where all the other layers' SVG info is removed
+        ''' 
+        animationFramePNGs = {}
+        # For each animation frame,
+        # remove all layers ('g' tags) that aren't 
+        # for the current animation frame (i),
+        # Save as a PNG image,
+        # Store list of all outputted frame PNGs
+        
+        for frame in range(1, len(layers.keys())+1):
+            #Copy root tree
+            rootCopy = root.__copy__()
 
-        # Position text in center of document
-        text.set('x', str(width / 2))
-        text.set('y', str(height / 2))
+            # Remove SVG for children that are layers and
+            # are not the current frame 
+            childrenToRemove = []
+            for childIndex in range(0, len(rootCopy.getchildren())):
+                child = rootCopy.getchildren()[childIndex]
 
-        # Set 'text-align' property on text
-        style = {'text-align' : 'center', 'text-anchor' : 'middle'}
-        text.set('style', formatStyle(style))
+                # If it is a layer
+                if child.tag == LAYER_TAG:
+                    # And it is NOT the current frame's layer
+                    if child != rootCopy.getchildren()[layers[str(frame)]]:
+                        childrenToRemove.append(rootCopy.getchildren()[childIndex])
 
-        layer.append(text)
+            for child in childrenToRemove:
+                rootCopy.remove(child)
+
+            svg_filepath = "%sFrame%s.svg" % (PREFIX, frame)
+            png_filepath = "%sFrame%s.png" % (PREFIX, frame)            
+
+            svg_xml = etree.tostring(rootCopy)
+            
+            self.saveSVG(svg_xml, svg_filepath)
+            self.convertSVGtoPNG(svg_filepath, png_filepath)
+
+            animationFramePNGs[frame] = png_filepath
+
+        imageFrames = []
+        for frame in animationFramePNGs:
+            filepath = animationFramePNGs[frame]
+            imageFrames.append(imageio.imread(filepath))
+        
+        
+        imageio.mimsave(GIF_FILE, imageFrames)
+        
+
+    def saveSVG(self, svg_xml, output_file):
+        with open(output_file, 'w') as f:
+            f.write(svg_xml)
+
+    def convertSVGtoPNG(self, svg_file, output_file):
+        #exe = "C:\\Program Files\\Inkscape\\inkscape.exe"
+        exe = "inkscape"
+
+        command = "%s -D -f \"%s\" -e \"%s\"" % (exe, svg_file, output_file)
+
+        p = subprocess.Popen(
+            command,
+            shell=True,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE)
+        
+        return_code = p.wait()
+        f = p.stdout
+        err = p.stderr
 
 # Create effect instance and apply it.
 DUMMY_EXTENSION = DummyExtension()
